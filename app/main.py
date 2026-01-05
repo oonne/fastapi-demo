bbd960964288aa0980a228bf87c8b2b6074f8454
JAY <JAY@oonne.com>
2026-01-05 14:51:10 +0800

    feat: 增加demo

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ from typing import Union
 
 from fastapi import FastAPI
 
-from app.routers import items
+from app.routers import demo, items
 
 app = FastAPI()
 
 # 注册路由
 app.include_router(items.router, prefix="/api", tags=["items"])
+app.include_router(demo.router, prefix="/api", tags=["demo"])
